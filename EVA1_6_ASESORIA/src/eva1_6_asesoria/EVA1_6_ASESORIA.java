18505c3b64ecf8a49cf25653d486b9272cc3029e
yis19 <65199911+yisus2099@users.noreply.github.com>
2022-03-07 01:46:20 -0700

    Add files via upload

---
+++ EVA1_6_ASESORIA/src/eva1_6_asesoria/EVA1_6_ASESORIA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_6_asesoria;
+
+/**
+ *
+ * @author Jesús Alejandro Torres Ramírez
+ */
+public class EVA1_6_ASESORIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = Integer.MAX_VALUE;
+        System.out.println("Valor =" + valor);
+        valor = valor +1;
+         System.out.println("Valor =" + valor);
+    }
+    
+}
