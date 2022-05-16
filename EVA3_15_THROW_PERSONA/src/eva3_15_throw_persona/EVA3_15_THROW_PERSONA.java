3ed0d789aa19c9aeb61e448c90217d8816d38721
yis19 <65199911+yisus2099@users.noreply.github.com>
2022-05-16 09:50:54 -0600

    Add files via upload

---
+++ EVA3_15_THROW_PERSONA/src/eva3_15_throw_persona/EVA3_15_THROW_PERSONA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_15_throw_persona;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_15_THROW_PERSONA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
