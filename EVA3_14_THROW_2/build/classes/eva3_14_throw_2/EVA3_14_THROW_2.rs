02aaf82575fe68809e4c73af5f8adf1d9b39e162
yis19 <jesusalejandrot958@gmail.com>
2022-05-16 23:52:21 -0600

    New

---
+++ EVA3_14_THROW_2/build/classes/eva3_14_throw_2/EVA3_14_THROW_2.rs
@@ -0,0 +1,2 @@
+eva3_14_throw_2.Persona
+eva3_14_throw_2.EVA3_14_THROW_2
--- EVA3_15_THROW_PERSONA/src/eva3_15_throw_persona/EVA3_15_THROW_PERSONA.java
+++ EVA3_15_THROW_PERSONA/src/eva3_15_throw_persona/EVA3_15_THROW_PERSONA.java
@@ -6,6 +6,9 @@
 
 package eva3_15_throw_persona;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author invitado
@@ -17,6 +20,27 @@ public class EVA3_15_THROW_PERSONA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+              boolean datoErroneo = true; //El usuario siempre se equivoca
+        Scanner input = new Scanner(System.in);
+        do {
+            try { 
+                System.out.println("Introduce tu edad(numero entero): ");
+                int iedad = input.nextInt();
+                if ((iedad >= 0) && (iedad <= 130))
+                    System.out.println("Tu edad es: " + iedad);
+ 
+                else{
+                    datoErroneo = false;
+                    throw new Exception("Rango de edad incorrecto!!");
+                    
+                    }
+            } catch (InputMismatchException e) {
+                System.out.println(input.next() + " no es un número válido");
+            } catch (Exception e){
+                System.out.println("Edad no valida");
+            }
+
+        } while (datoErroneo);
     }
     
 }
