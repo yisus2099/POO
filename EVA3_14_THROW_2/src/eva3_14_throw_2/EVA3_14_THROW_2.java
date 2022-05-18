ea57a2dc0abe12469fc14f5be55aac89a0941f8d
yis19 <jesusalejandrot958@gmail.com>
2022-05-17 18:27:07 -0600

    New

--- EVA3_14_THROW_2/src/eva3_14_throw_2/EVA3_14_THROW_2.java
+++ EVA3_14_THROW_2/src/eva3_14_throw_2/EVA3_14_THROW_2.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package eva3_14_throw_2;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
