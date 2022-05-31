522ea3b03312448fc3c178970fca82b433ad5fbd
yis19 <65199911+yisus2099@users.noreply.github.com>
2022-05-31 09:16:28 -0600

    Update EVA3_19_BUFFERS.java

--- EVA3_19_BUFFERS/src/eva3_19_buffers/EVA3_19_BUFFERS.java
+++ EVA3_19_BUFFERS/src/eva3_19_buffers/EVA3_19_BUFFERS.java
@@ -54,6 +54,7 @@ public class EVA3_19_BUFFERS {
                 System.out.println(linea);
                 linea = leerTexto.readLine();
             }
+                leerTexto.close();//Cerrar el stream
         } catch (IOException ex) {
             Logger.getLogger(EVA3_19_BUFFERS.class.getName()).log(Level.SEVERE, null, ex);
         }
