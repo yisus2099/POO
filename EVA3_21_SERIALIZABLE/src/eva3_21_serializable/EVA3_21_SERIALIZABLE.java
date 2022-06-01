bee4d8e480d01284fcdc1c246068067e8c58e445
yis19 <65199911+yisus2099@users.noreply.github.com>
2022-06-01 09:50:38 -0600

    Add files via upload

---
+++ EVA3_21_SERIALIZABLE/src/eva3_21_serializable/EVA3_21_SERIALIZABLE.java
@@ -0,0 +1,96 @@
+
+package eva3_21_serializable;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jesús Alejandro Torres Ramírez
+ */
+public class EVA3_21_SERIALIZABLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Binarios y de texto
+        //ESPECIFICAR QUE OBJETOS (CLASES) SON SERIALIZABLES
+        //DEBEMOS HACER QUE LA CLASE IMPLEMENTE LA INTERFAZ SERIALIZABLE
+        Persona persona = new Persona("Pedro", "Páramo");
+        escribirObj(persona);
+        leerObj();
+    }
+    
+    //GUARDAR OBJETOS
+    public static void escribirObj(Object obj){
+        try {
+            FileOutputStream abrirArch = new FileOutputStream("c:/ARCHIVOS/archivo3.obj");
+            try {
+                ObjectOutputStream guardarObj = new ObjectOutputStream(abrirArch);
+                guardarObj.writeObject(obj);
+                guardarObj.flush();
+                guardarObj.close();
+            } catch (IOException ex) {
+                Logger.getLogger(EVA3_21_SERIALIZABLE.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(EVA3_21_SERIALIZABLE.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    //LEER OBJETOS
+     public static void leerObj(Object obj){
+        try {
+                FileInputStream abrirArch = new FileInputStream("c:/ARCHIVOS/archivo3.obj");
+                ObjectInputStream leerObjeto = new ObjectInputStream(abrirArch);
+                Persona persona = (Persona)leerObjeto.readObject();//OBJETO (Class Object)
+                System.out.println("Nombre: " + persona.getNombre() + " " + persona.getApellido());//Falta que CORRAAAAAAAAAAAAA
+               
+            }catch (FileNotFoundException ex) {
+                Logger.getLogger(EVA3_21_SERIALIZABLE.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException | ClassNotFoundException ex) {
+                Logger.getLogger(EVA3_21_SERIALIZABLE.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        } 
+    }
+
+
+class Persona implements Serializable{
+    private String nombre;
+    private String apellido;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+}
---
+++ EVA3_22_SERIALIZABLE_ARREGLOS/src/eva3_22_serializable_arreglos/EVA3_22_SERIALIZABLE_ARREGLOS.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_22_serializable_arreglos;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_22_SERIALIZABLE_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Persona> miListaPerso = new ArrayList(); //Collections <TIPOS DE DATOS GENERICOS>
+        //JAVA --> LISTAS, ARREGLOS, ETC: 0 --> N- 1
+        miListaPerso.add(new Persona ("Juan", "Perez"));//0
+        miListaPerso.add(new Persona ("Pedro", "Perez"));//1
+        miListaPerso.add(new Persona ("Ana", "Perez"));//2
+        miListaPerso.add(new Persona ("Florentino", "Perez"));
+        miListaPerso.add(new Persona ("Antonio", "Perez"));
+        miListaPerso.add(new Persona ("Espiridion", "Perez"));
+        miListaPerso.add(new Persona ("Paz", "Perez"));
+        escribirObj(miListaPerso);
+        leerObj(); 
+        
+    }
+    public static void escribirObj(Object obj){
+        try {
+            FileOutputStream abrirArch = new FileOutputStream("c:/ARCHIVOS/archivo3.obj");
+            try {
+                ObjectOutputStream guardarObj = new ObjectOutputStream(abrirArch);
+                guardarObj.writeObject(obj);
+                guardarObj.flush();
+                guardarObj.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } catch (FileNotFoundException ex) {
+           ex.printStackTrace();
+        }
+        
+    }
+    
+    public static void leerObj(Object obj){
+        try {
+                FileInputStream abrirArch = new FileInputStream("c:/ARCHIVOS/archivo3.obj");
+                ObjectInputStream leerObjeto = new ObjectInputStream(abrirArch);
+                ArrayList<Persona> miListaPerso = (ArrayList<Persona>)leerObjeto.readObject();//OBJETO (Class Object)
+                for(int i = 0; i < miListaPerso.size(); i++){
+                    Persona persona = miListaPerso.get(i);
+                System.out.println("Nombre: " + persona.getNombre() + " " + persona.getApellido());
+                }
+            }catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException | ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        
+        } 
+    }
+
+
+
+class Persona implements Serializable{
+    private String nombre;
+    private String apellido;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+}
